56a69678a68d8bf24e689bf233a81e795aeed24f
Jonas Krger <s198565@stud.hioa.no>
2017-09-06 14:11:11 +0200

    Cleaned up, reverted to shift; outside and checking if arguments were correct. This removed 4 if sentences later on.

--- assignment2/calc.sh
+++ assignment2/calc.sh
@@ -3,53 +3,40 @@
 
 argument=$1 #Adds first argument after ./calc.sh to $argument
 if [[ $# -gt 0 ]]; then
-    if [[ "$argument" == "S" ]]; then
-        sum=0
-        shift;
+    shift;
+    if [[ "$argument" == "P" ]] || [[ "$argument" == "S" ]] || [[ "$argument" == "M" ]] || [[ "$argument" == "m" ]]; then
         if [[ $# -eq 0 ]]; then
             echo "There must be one or more numbers for this to work."
         else
-            for var in $@; do
-                ((sum+=var))
-            done
-            echo "Sum = $sum"
-        fi
-    elif [[ "$argument" == "P" ]]; then
-        sum=1 #Need to have it on 1, otherwise it would all get a product of 0
-        shift;
-        if [[ $# -eq 0 ]]; then
-            echo "There must be one or more numbers for this to work."
-        else
-            for var in $@; do
-                ((sum*=var))
-            done
-            echo "Product = $sum"
-        fi
-    elif [[ "$argument" == "M" ]]; then
-        shift;
-        storst=$1
-        if [[ $# -eq 0 ]]; then
-            echo "There must be one or more numbers for this to work."
-        else
-            for var in $@; do
-                if [[ $var -gt $storst ]]; then
-                    storst=$var
-                fi
-            done
-        echo "Maximum = $storst"
-        fi
-    elif [[ "$argument" == "m" ]]; then
-        shift;
-        minst=$1
-        if [[ $# -eq 0 ]]; then
-            echo "There must be one or more numbers for this to work."
-        else
-            for var in $@; do
-                if [[ $var -lt $minst ]]; then
-                    minst=$var
-                fi
-            done
-        echo "Minimum = $minst"
+            if [[ "$argument" == "S" ]]; then
+                sum=0
+                for var in $@; do
+                    ((sum+=var))
+                done
+                echo "Sum = $sum"
+            elif [[ "$argument" == "P" ]]; then
+                sum=1 #Need to have it on 1, otherwise it would all get a product of 0
+                for var in $@; do
+                    ((sum*=var))
+                done
+                echo "Product = $sum"
+            elif [[ "$argument" == "M" ]]; then
+                storst=$1
+                for var in $@; do
+                    if [[ $var -gt $storst ]]; then
+                        storst=$var
+                    fi
+                done
+                echo "Maximum = $storst"
+            elif [[ "$argument" == "m" ]]; then
+                minst=$1
+                for var in $@; do
+                    if [[ $var -lt $minst ]]; then
+                        minst=$var
+                    fi
+                done
+                echo "Minimum = $minst"
+            fi
         fi
     else
       echo "First argument must be either S, P, M or m"
