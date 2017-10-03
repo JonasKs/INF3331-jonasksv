3c62bf2589baf6b46cf0a395fed98870c728ccf1
jonasksv <31506932+jonasksv@users.noreply.github.com>
2017-10-03 16:36:36 +0200

    Create integrator.py

---
+++ assignment4/integrator.py
@@ -0,0 +1,23 @@
+class Integrator:
+    def integrate(f, a, b, N):
+        width = float(b-a)/N
+        areal = float(0)
+        for i in range(N):
+            areal += f(a + i*width)
+        return (areal * width)
+
+    def plot_dat(f, a, b, N):
+        n = np.linspace(a,b,N)
+        plt.plot(n,f(n),color='blue')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Numerical approximation: Rectangular')
+        for i in range(1,len(n)):
+            c = n[i-1]
+            d = n[i]
+            plt.plot([c,c],[0,f((c+d)/2)],color='black')
+            plt.plot([d,d],[0,f(d)],color='black')
+            plt.plot([c,d],[f((c+d)/2),f((c+d)/2)],color='black')
+        plt.savefig('quadratic_error.png') #Show is the "end of the plot", so this must be before we show the plot.
+        plt.show()
+        return 0
