afe3b0e5e3580c10d2f0d42742a2bbce01a22b6b
Jonas Krüger Svensson <jonasksv@ifi.uio.no>
2017-10-05 20:32:41 +0200

    Adding some variables to setup.py for task 4.7

--- assignment4/setup.py
+++ assignment4/setup.py
@@ -1,7 +1,12 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 from Cython.Build import cythonize
 import numpy
 
 setup(
+    name = "integrator_package",
+    author = "Jonas Kruger Svensson",
     ext_modules=cythonize("*.pyx"),
+    packages=find_packages(),
+    python_requires='>=3'
+    include_dirs = ["."]
 )
