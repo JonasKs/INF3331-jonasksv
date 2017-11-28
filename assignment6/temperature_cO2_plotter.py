3c6cd29aa3b7439f5f557442a0c67689115488be
Jonas Krüger Svensson <jonasksv@ifi.uio.no>
2017-11-28 14:17:11 +0100

    Task 6.4 done

--- assignment6/temperature_cO2_plotter.py
+++ assignment6/temperature_cO2_plotter.py
@@ -2,10 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+import numpy as np
 
 def plot_CO2(yearfrom=1751, yearto=2012):
     """Plot CO2 values from year x to year"""
-    csvfile = '/home/ubuntu/Documents/inf3331/co2.csv'
+    csvfile = 'co2.csv'
     contains = pd.read_csv(csvfile, sep=',')
     contains = contains.set_index('Year')
     years = contains.loc[yearfrom:yearto, 'Carbon']
@@ -26,7 +27,7 @@ def plot_CO2(yearfrom=1751, yearto=2012):
 
 def plot_temperature(month, yearfrom=1816, yearto=2012, ymin=None, ymax=None):
     """Plot CO2 levels for specified month from year x to year x"""
-    csvfile = '/home/ubuntu/Documents/inf3331/temperature.csv'
+    csvfile = 'temperature.csv'
     contains = pd.read_csv(csvfile, sep=',')
     contains = contains.set_index('Year')
     years = contains.loc[yearfrom:yearto, month]
@@ -45,3 +46,29 @@ def plot_temperature(month, yearfrom=1816, yearto=2012, ymin=None, ymax=None):
     return figdata_png
 
 #plot_temperature('September', 1816, 1888)
+
+def plot_co2_by_country(year,lower=0,upper=10):
+    """Plot CO2 by country, with upper/lower treshold."""
+    csvfile = "CO2_by_country.csv"
+    contains = pd.read_csv(csvfile, sep=',')
+    contains = contains[["Country Code", str(year)]].dropna()
+    contains = contains.set_index('Country Code')
+    scale = contains.loc[contains[str(year)] > lower]
+    scale = scale.loc[scale[str(year)] < upper]
+    objects = scale.index.tolist()
+    scale = scale[str(year)].tolist()
+    #Plotting.
+    y_pos = np.arange(len(objects))
+    plt.bar(y_pos, scale)
+    plt.xticks(y_pos, objects)
+    plt.title('CO2 countries in year {} between values {} and {}'.format(year,lower,upper))
+
+    #Make Matplotlib write to BytesIO
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    #figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue()).decode("ascii")
+    plt.close() # Had to be done to clean figure from memory
+    return figdata_png
+
+#plot_co2_by_country(1960,6,10)
--- assignment6/web_visualization.py
+++ assignment6/web_visualization.py
@@ -1,29 +1,44 @@
 from flask import Flask, render_template, request
-from temperature_cO2_plotter import plot_temperature, plot_CO2
+from temperature_cO2_plotter import plot_temperature, plot_CO2, plot_co2_by_country
 app = Flask(__name__)
 
 @app.route("/", methods=['POST','GET'])
 def plotting():
+    """Plot the histograms through the website. Please note that not passing
+    ymin and ymax values will not work.
+
+    """
     if request.method == 'POST':
         # Acces the form data:
         #plot_CO2
         fromyear = request.form["fromyear"]
         toyear = request.form["toyear"]
         #plot_temperature
-        ymin = request.form["ymin"]
-        ymax = request.form["ymax"]
+        #Check if ymin is empty, and if it is, make it None instead.
+        if request.form["ymin"] or request.form["ymax"] is "":
+            ymin = None
+            ymax = None
+        else: #If not empty, make it an int to prevent a crash.
+            ymin = (int(request.form["ymin"]))
+            ymax = (int(request.form["ymax"]))
+        #We assume everything else is filled out, to not over complicate things.
         fromyear2 = request.form["fromyear2"]
         toyear2 = request.form["toyear2"]
         month = request.form["month"]
+        year = int(request.form["year"])
+        lower = int(request.form["lower"])
+        upper = int(request.form["upper"])
 
+        plot_country = plot_co2_by_country(year,lower,upper)
         plot_temp = plot_temperature(month, fromyear2, toyear2, ymin, ymax)
         plot_co2 = plot_CO2(fromyear,toyear)
-        return render_template('plot.html', co2=plot_co2, temp=plot_temp )
+        return render_template('plot.html', co2=plot_co2, temp=plot_temp, country=plot_country)
 
     if request.method == 'GET':
         plot_co2 = plot_CO2(1755,2012)
-        plot_temp = plot_temperature('September')
-        return render_template('plot.html', co2=plot_co2, temp=plot_temp )
+        plot_temp = plot_temperature('January')
+        plot_country = plot_co2_by_country(1960,6,10)
+        return render_template('plot.html', co2=plot_co2, temp=plot_temp, country=plot_country )
 
 
 
